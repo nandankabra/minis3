492b7bd6156314e75ebb2ff9a8c39232111d0a35
Nandan Kabra <nandankabra2@gmail.com>
2026-09-19 00:32:32 +0530

    update

--- src/main/java/com/misi3/storage/StorageNodeController.java
+++ src/main/java/com/misi3/storage/StorageNodeController.java
@@ -1,13 +1,12 @@
 package com.misi3.storage;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/storage")
 public class StorageNodeController {
-    @GetMapping("/health")
+
+    @GetMapping("/storage/health")
     public String health() {
         return "Storage Node Running";
     }
